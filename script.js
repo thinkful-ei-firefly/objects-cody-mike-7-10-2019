4181aff2bb4ce014d29a4110a794b645df1eed09
m-cal <iengineersoftware@gmail.com>
2019-07-10 11:26:16 -0400

    finished object drills 1

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'hello';
+    }
+  };
+}
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+function personMaker() {
+  const person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    }
+  };
+  return person;
+}
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+  
